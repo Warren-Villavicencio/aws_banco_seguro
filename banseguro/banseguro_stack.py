ae1d5a82ed93db5b2b114f724e67b5785e520c27
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-06-27 17:56:57 -0500

    Cambios en el stack

--- banseguro/banseguro_stack.py
+++ banseguro/banseguro_stack.py
@@ -7,12 +7,46 @@ from aws_cdk import (
     aws_sns as sns,
     aws_sns_subscriptions as subs,
 )
+from aws_cdk import (
+    aws_rds as rds,
+    aws_secretsmanager as secretsmanager,
+    Stack,
+    Construct,
+)
 
+from aws_cdk import (
+    aws_apigateway as apigw,
+    Stack,
+    Construct,
+)
 
 class BanseguroStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+
+        db_credentials_secret = secretsmanager.Secret(self, "DBCredentialsSecret")
+        database = rds.DatabaseInstance(
+            self,
+            "Database",
+            engine=rds.DatabaseInstanceEngine.mysql(
+                version=rds.MysqlEngineVersion.VER_8_0_19
+            ),
+            credentials=rds.Credentials.from_secret(db_credentials_secret),
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
+            ),
+            allocated_storage=20,
+            database_name="banco_seguro",
+        )
+
+
+   # Define the API Gateway
+        api = apigw.RestApi(self, "banco_seguro_api")
+
+        # Define the API Gateway resources and methods
+        cuentas = api.root.add_resource
 
         queue = sqs.Queue(
             self, "BanseguroQueue",
